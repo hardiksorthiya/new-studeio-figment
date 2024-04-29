e5c5c84d3fde458e2b53bdf18400755dd22650bd
hardiksorthiya <sorathiyahardik000@gmail.com>
2024-04-29 11:55:31 +0530

    final touch 29/04

--- js/script.js
+++ js/script.js
@@ -1,3 +1,23 @@
+$(document).ready(function(){
+  $('.menu-close').hide();
+
+  $('.menu').click(function(){
+      $('.menu-panel').toggleClass('show');
+      $('.menu-background').toggleClass('show');
+      $('.menu').hide();
+      $('.menu-close').show();
+  });
+  $('.menu-close').click(function(){
+      $('.menu-panel').removeClass('show');
+      $('.menu-background').removeClass('show');
+      $('.menu').show();
+      $('.menu-close').hide();
+  });
+});
+
+
+
+
 // swipe slider
 
 
